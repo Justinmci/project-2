329621de74497124635f8680da92acea498c6316
wiffin <wiffinian@gmail.com>
2022-03-01 15:35:16 -0500

    add sessions to connection and set up sessions for users

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session')
 const routes = require('./controllers');
 // import sequelize connection
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Setup Express App
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Setup Sessions
+const sess = {
+  secret: process.env.SESS_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 // Sets up the Express app to handle data parsing
 app.use(express.json());
