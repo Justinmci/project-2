abf6a3a8861267e236aa057b4820d6c4a2a293fd
Cysmi <cnb10100@gmail.com>
2022-03-01 20:59:47 -0500

    remove console log

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,6 @@ const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
-  console.log(req.session);
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });    
 
