f92d91eb591d6e1c60355f45f536d4490b57d373
Cysmi <cnb10100@gmail.com>
2022-03-06 04:22:38 -0500

    testing heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 // Setup Sessions
 const sess = {
-  secret: process.env.SESS_SECRET,
+  secret: 'super secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
