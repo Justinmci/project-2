f5b434f0466eff024614fac13691a622732b65f9
Cysmi <cnb10100@gmail.com>
2022-03-05 20:17:29 -0500

    updated routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,11 @@
 const path = require("path");
 const sequelize = require('../config/connection');
+const { Post, User, Comment, Likes } = require('../models');
 const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
-});  
-
-router.get("/signup", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/signup.html"));
 });  
 
 router.get('/login', (req, res) => {
@@ -17,6 +14,92 @@ router.get('/login', (req, res) => {
     return;
   }
   res.sendFile()
+});
+
+// get all posts for explore
+router.get('/explore', (req, res) => {
+  Post.findAll({
+    attributes: [
+      'id',
+      'content',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM likes WHERE post.id = likes.post_id)'), 'likes_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.sendFile('explore.html', {
+        posts,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get single post
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'content',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM likes WHERE post.id = liked.post_id)'), 'likes_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      const post = dbPostData.get({ plain: true });
+
+      res.sendFile('story.html', {
+        post,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router
